06ef5a0dca38b4824fc6ed0bf941c532caa2a9f7
Helge Jensen <hej@scadaminds.com>
2017-09-30 11:15:05 +0200

    Tests for RhinoMocks

---
+++ RhinoTests/RhinoTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace RhinoTests
+{
+    public class RhinoTests
+    {
+        public class Foo
+        {
+            public virtual string Bar { get; set; }
+        }
+        [Test]
+        public static void TestMockPropertyInsideRepo()
+        {
+            var mockRepo = new MockRepository();
+            var foo = mockRepo.DynamicMock<Foo>();
+            long i = 0;
+            Func<string> cb = () => Interlocked.Increment(ref i).ToString();
+            foo.Stub(x => x.Bar)
+                .WhenCalled(mi => mi.ReturnValue = cb())
+                .Return(default(string));
+            mockRepo.ReplayAll();
+            foo.Bar.Should().Be("1");
+            foo.Bar.Should().Be("2");
+        }
+        [Test]
+        public static void TestMockPropertyOnGlobal()
+        {
+            var foo = MockRepository.GenerateMock<Foo>();
+            long i = 0;
+            Func<string> cb = () => Interlocked.Increment(ref i).ToString();
+            foo.Stub(x => x.Bar)
+                .WhenCalled(mi => mi.ReturnValue = cb())
+                .Return(default(string));
+            foo.Bar.Should().Be("1");
+            foo.Bar.Should().Be("2");
+        }
+    }
+}
